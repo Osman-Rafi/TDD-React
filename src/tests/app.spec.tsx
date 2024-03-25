300f4d0aa49c5badd4afb271fbaf59507e2a088a
Smooth Criminal <33856265+Osman-Rafi@users.noreply.github.com>
2024-03-25 14:59:05 +0600

    setting up test env

---
+++ src/tests/app.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+
+import App from "../App";
+
+test("should render the App component", () => {
+  render(<App />);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxInject: "import React from 'react'",
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
   plugins: [react()],
-})
+});
